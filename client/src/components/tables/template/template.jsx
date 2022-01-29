ed7186f8aaf2a016ac63772a4444c10cb5499fa8
Bryan alvarez <bryan.alvarez@gmail.com>
2022-01-29 14:22:57 -0800

    connecting to public repo

--- client/src/components/tables/template/template.jsx
+++ client/src/components/tables/template/template.jsx
@@ -2,87 +2,15 @@ import React, { useMemo } from 'react'
 
 import { 
     useTable, 
-<<<<<<< HEAD
-    // useFilters,
-    // useGlobalFilter,
-    // useAsyncDebounce 
-} from 'react-table';
-
-=======
-    useFilters,
-    useGlobalFilter 
+    useFilters 
 } from 'react-table';
 
 import GlobalFilter from './global-filter';
 
->>>>>>> ec7fea9a3405a91f2d32c719ef41f0dbe4031e8d
 import './template.styles.css';
 
 const TableTemplate = ({ columns, data, customClass }) => {
 
-<<<<<<< HEAD
-    // React.useMemo(
-    //     () => ({
-    //       text: (rows, id, filterValue) => {
-    //         return rows.filter(row => {
-    //           const rowValue = row.values[id]
-    //           return rowValue !== undefined
-    //             ? String(rowValue)
-    //                 .toLowerCase()
-    //                 .startsWith(String(filterValue).toLowerCase())
-    //             : true
-    //         })
-    //       },
-    //     }),
-    //     []
-    //   );
-
-    // function GlobalFilter({
-    //     preGlobalFilteredRows,
-    //     globalFilter,
-    //     setGlobalFilter,
-    //   }) {
-    //     const count = preGlobalFilteredRows.length
-    //     const [value, setValue] = useState(globalFilter)
-    //     const onChange = useAsyncDebounce(value => {
-    //       setGlobalFilter(value || undefined)
-    //     }, 200)
-
-    //     return (
-    //         <span>
-    //           Search:{' '}
-    //           <input
-    //             value={value || ""}
-    //             onChange={e => {
-    //               setValue(e.target.value);
-    //               onChange(e.target.value);
-    //             }}
-    //             placeholder={`${count} records...`}
-    //             style={{
-    //               fontSize: '1.1rem',
-    //               border: '0',
-    //               padding: '3px 5px',
-    //               width: '60%'
-    //             }}
-    //           />
-    //         </span>
-    //       )
-    //     };
-
-    // function DefaultColumnFilter({ column: { filterValue, preFilteredRows, setFilter },}) {
-    //     const count = preFilteredRows.length
-        
-    //     return (
-    //         <input
-    //         value={filterValue || ''}
-    //         onChange={e => { setFilter(e.target.value || undefined)}}
-    //         placeholder={`Search ${count} records...`}
-    //         />
-    //     );
-    // };
-
-    // const defaultColumn = useMemo(() => ({ Filter: DefaultColumnFilter }),[]);
-=======
     useMemo(
         () => ({
           text: (rows, id, filterValue) => {
@@ -112,7 +40,7 @@ const TableTemplate = ({ columns, data, customClass }) => {
     };
 
     const defaultColumn = useMemo(() => ({ Filter: DefaultColumnFilter }),[]);
->>>>>>> ec7fea9a3405a91f2d32c719ef41f0dbe4031e8d
+
 
     const {
         getTableProps,
@@ -120,42 +48,23 @@ const TableTemplate = ({ columns, data, customClass }) => {
         headerGroups,
         rows,
         prepareRow,
-<<<<<<< HEAD
-        // state,
-        // preGlobalFilteredRows,
-        // setGlobalFilter,
-=======
         state,
         preGlobalFilteredRows,
         setGlobalFilter,
->>>>>>> ec7fea9a3405a91f2d32c719ef41f0dbe4031e8d
       } = useTable(
         {
           columns,
           data,
-<<<<<<< HEAD
-        //   defaultColumn,
-        },
-        // useFilters, 
-        // useGlobalFilter 
-=======
           defaultColumn,
         },
         useFilters, 
         useGlobalFilter 
->>>>>>> ec7fea9a3405a91f2d32c719ef41f0dbe4031e8d
+
       )
 
     return (
             <table {...getTableProps()} className={`custom-table ${customClass}`}>
             <thead>
-<<<<<<< HEAD
-                {/* <GlobalFilter
-                    preGlobalFilteredRows={preGlobalFilteredRows}
-                    globalFilter={state.globalFilter}
-                    setGlobalFilter={setGlobalFilter}
-                /> */}
-=======
                 <tr>
                     <th style={{padding: 0}}>
                         <GlobalFilter
@@ -165,7 +74,7 @@ const TableTemplate = ({ columns, data, customClass }) => {
                         />
                     </th>
                 </tr>
->>>>>>> ec7fea9a3405a91f2d32c719ef41f0dbe4031e8d
+
                 {
                 headerGroups.map(headerGroup => (
                     <tr {...headerGroup.getHeaderGroupProps()}>
